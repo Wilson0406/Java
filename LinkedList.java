ace0da6e2308df820be304b0bf1f8d2583ecefb3
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-03-04 01:57:13 +0530

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+    public static class Node {
+        int data;
+        Node node;
+    }
+  
+  public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+  }
+}

e2fb62d7e851e3df971d6015826410f71e731658
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-08-05 00:36:25 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (56.63%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -10,15 +10,19 @@ class Solution {
         
         // while(out.size() < n*m) {
         while(left <= right && up <= down) {
+            // left to right
             for(int i = left; i <= right && out.size() < n*m; i++)
                 out.add(matrix[up][i]);
             
+            // top to bottom 
             for(int j = up + 1; j <= down - 1 && out.size() < n*m; j++)
                 out.add(matrix[j][right]);
             
+            // right to left
             for(int i = right; i >= left && out.size() < n*m; i--)
                 out.add(matrix[down][i]);
             
+            // bottom to up
             for(int j = down - 1; j >= up + 1 && out.size() < n*m; j--)
                 out.add(matrix[j][left]);
             

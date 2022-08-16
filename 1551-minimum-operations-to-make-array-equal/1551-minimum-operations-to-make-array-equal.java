849218e882960807c2c31422c709f27c71b1e2f8
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-08-17 02:52:30 +0530

    Time: 1 ms (89.38%), Space: 41.2 MB (44.69%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
@@ -1,25 +1,28 @@
 class Solution {
     public int minOperations(int n) {
-        int[] arr = new int[n];
-        int sum = 0;
-        for(int i = 0; i < n; i++) {
-            arr[i] = (2*i) + 1;
-        }
-        for(int i = 0; i < n; i++) {
-            sum += arr[i];
-        }
-        int count;
-        if(n % 2 == 0) {
-            count = 1;
-            for(int i = n - 1; i > (n+1)/2; i--) {
-                count = count + (arr[i] - n);
-            }
-        } else {
-            count = 0;
-            for(int i = n - 1; i > n/2; i--) {
-                count = count + (arr[i] - n);
-            }
-        }
-        return count;
+        // int[] arr = new int[n];
+        // int sum = 0;
+        // for(int i = 0; i < n; i++) {
+        //     arr[i] = (2*i) + 1;
+        // }
+        // for(int i = 0; i < n; i++) {
+        //     sum += arr[i];
+        // }
+        // int count;
+        // if(n % 2 == 0) {
+        //     count = 1;
+        //     for(int i = n - 1; i > (n+1)/2; i--) {
+        //         count = count + (arr[i] - n);
+        //     }
+        // } else {
+        //     count = 0;
+        //     for(int i = n - 1; i > n/2; i--) {
+        //         count = count + (arr[i] - n);
+        //     }
+        // }
+        // return count;
+        
+        // LOLLLLLLLL
+        return (n*n)/4;
     }
 }

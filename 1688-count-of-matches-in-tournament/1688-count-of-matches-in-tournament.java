20941b3d05cd8f854624446f934a8eb4caa115b6
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-08-17 02:11:38 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (92.43%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -1,20 +1,23 @@
 class Solution {
     public int numberOfMatches(int n) {
-        if(n < 2)
-            return 0;
-        int m=0, a=0;
-        int tot = 1;
-        while(n!=2) {
-            if((n & 1) != 1) {
-                m = n>>1;
-                a = n>>1;
-            } else {
-                m = (n-1)>>1;
-                a = m + 1;
-            }
-            n = a;
-            tot += m;
-        }
-        return tot;
+        // if(n < 2)
+        //     return 0;
+        // int m=0, a=0;
+        // int tot = 1;
+        // while(n!=2) {
+        //     if((n & 1) != 1) {
+        //         m = n>>1;
+        //         a = n>>1;
+        //     } else {
+        //         m = (n-1)>>1;
+        //         a = m + 1;
+        //     }
+        //     n = a;
+        //     tot += m;
+        // }
+        // return tot;
+        
+        //LOL super hard
+        return n - 1;
     }
 }

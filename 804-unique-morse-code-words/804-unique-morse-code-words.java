b25701f1a5abf035856359879215f81606fe39d1
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-08-17 20:46:57 +0530

    Time: 7 ms (14.08%), Space: 42.3 MB (44.14%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.java
+++ 804-unique-morse-code-words/804-unique-morse-code-words.java
@@ -1,16 +1,24 @@
 class Solution {
     public int uniqueMorseRepresentations(String[] words) {
+        //morse code for each alphabet
         String[] morse = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        
+        //alphabet string
         String alp = "abcdefghijklmnopqrstuvwxyz";
+        //array of alphabets
         char[] alpArr = alp.toCharArray();
+        //array containing morse code for all the strings in the array
         String[] ans = new String[words.length];
         
-        HashMap<String, Integer> map = new HashMap<>();
+        //map to store  position of each alphabet(a->0, b->1, c->2...)
         HashMap<Character, Integer> letters = new HashMap<>();
-        
         for(int i = 0; i < 26; i++) {
             letters.put(alpArr[i],i);
         }
+        
+        //map to store count of each morse code in the answer array
+        HashMap<String, Integer> map = new HashMap<>();
+        
         int j = 0;
         for(String word: words){
             StringBuilder str = new StringBuilder();
@@ -18,16 +26,14 @@ class Solution {
                 char ch = word.charAt(i);
                 str.append(morse[letters.get(ch)]);
             }
-            ans[j] = str.toString();
-            j++;
+            ans[j++] = str.toString();
         }
-        
-        int count = 0;
         
         for(String word: ans) {
             map.put(word, map.getOrDefault(word,0)+1);
         }
-        count = map.size();
-        return count;
+        
+        //return unique morse code
+        return map.size();
     }
 }

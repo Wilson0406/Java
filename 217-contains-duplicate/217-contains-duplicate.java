6943680eee29de0e8a4e4f4e78dcba74c3d48baa
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-09-07 01:23:11 +0530

    Time: 15 ms (50.09%), Space: 69.9 MB (47.66%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,3 +1,4 @@
+// TC - O(n^2)
 // class Solution {
 //     public boolean containsDuplicate(int[] nums) {
 //             for(int i=0;i<nums.length;i++)
@@ -7,11 +8,26 @@
 //     }
 // }
 
+// TC - O(n)
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+//         for(int i=0;i<nums.length-1;i++)
+//             if(nums[i]==nums[i+1]) return true;
+//         return false;
+//     }
+// }
+
+// TC - O(n) SC - O(n)
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++)
-            if(nums[i]==nums[i+1]) return true;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            if(map.get(nums[i]) > 1)
+                return true;
+        }
+        
         return false;
     }
 }

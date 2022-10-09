b13dd09ced7016f57abb5dc936663973a6c17189
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2022-10-09 10:09:29 +0530

    Time: 2 ms (78.14%), Space: 54.6 MB (28.32%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,19 +1,33 @@
+// class Solution {
+//     public int largest(int arr[], int l) {
+//         int r = arr.length;
+//         int max = 0;
+//         for(int i = l;i < r; i++) {
+//             max = Math.max(max,arr[i]);
+//         }
+//         return max;
+//     }
+//     public int[] replaceElements(int[] arr) {
+//         int len = arr.length;
+//         int ans[] = new int[len];
+//         for(int i = 0; i < len - 1; i++) {
+//             ans[i] = largest(arr,i+1);
+//         }
+//         ans[len - 1] = -1;
+//         return ans;
+//     }
+    
+// }
+
+
 class Solution {
-    public int largest(int arr[], int l) {
-        int r = arr.length;
-        int max = 0;
-        for(int i = l;i < r; i++) {
-            max = Math.max(max,arr[i]);
-        }
-        return max;
-    }
     public int[] replaceElements(int[] arr) {
         int len = arr.length;
         int ans[] = new int[len];
-        for(int i = 0; i < len - 1; i++) {
-            ans[i] = largest(arr,i+1);
+        int max = -1;
+        for(int i = len - 1; i >= 0; i--) {
+            max = Math.max(arr[i], ans[i] = max);
         }
-        ans[len - 1] = -1;
         return ans;
     }
     

d612961aabd67ae4b563262528524d1a46399346
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2022-10-17 16:33:19 +0530

    Time: 12 ms (6.72%), Space: 42.1 MB (45.15%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < sentence.length(); i++) {
+            char ch = sentence.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        if(map.size() > 25) {
+            return true;
+        }
+        return false;
+    }
+}

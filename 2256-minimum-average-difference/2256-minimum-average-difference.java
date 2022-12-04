c442cb2b25b5f2ec3868ca97042f8e016a10152b
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2022-12-04 15:45:15 +0530

    Time: 28 ms (36.46%), Space: 76 MB (45.42%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.java
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -1,60 +1,26 @@
-// class Solution {
-//     public int minimumAverageDifference(int[] nums) {
-//         long min = Integer.MAX_VALUE;
-//         int sum1 = 0;
-//         int sum2 = 0;
-//         int n = nums.length;
-//         int res = 0;
-        
-//         for(int i = 0; i < n; i++) {
-//             sum2 += nums[i];
-//         }
-        
-//         for(int i = 0; i < n; i++) {
-//             sum1 += nums[i];
-//             sum2 -= nums[i];
-//             long abs = Math.abs(sum1/(i+1) - (i == n - 1 ? 0 : sum2/(n-i-1)));
-//             if(abs < min) {
-//                 min = abs;
-//                 res = i;
-//             }
-//         }
-        
-//         return res;
-//     }
-// }
-
-
-
-// // nums = [2,5,3,9,5,3]
-// // prefix = [2,7,10,19,24,27]
-// // [0,2,7,10,19,24]
-// // suffix = [25,20,17,8,3,0]
-
-
-
-
-
-
-
-
-
-
 class Solution {
     public int minimumAverageDifference(int[] nums) {
-        int len = nums.length, res = 0;
-        long min = Integer.MAX_VALUE, sum = 0, leftSum = 0, rightSum = 0;
-        for (int num : nums)
-            sum += num;
-        for (int i = 0; i < len; i++) {
-            leftSum += nums[i];
-            rightSum = sum - leftSum;
-            long diff = Math.abs(leftSum / (i + 1) - (len - i == 1 ? 0 : rightSum / (len -i - 1)));
-            if (diff < min) {
-                min = diff;
+        int n = nums.length, res = 0;
+        long min = Integer.MAX_VALUE, sum1 = 0, sum2 = 0, sum = 0;
+        for(int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        
+        for(int i = 0; i < n; i++) {
+            sum1 += nums[i];
+            sum2 = sum - sum1;
+            long abs = Math.abs(sum1/(i+1) - (n - i == 1 ? 0 : sum2/(n-i-1)));
+            if(abs < min) {
+                min = abs;
                 res = i;
             }
         }
+        
         return res;
     }
 }
+
+// nums = [2,5,3,9,5,3]
+// prefix = [2,7,10,19,24,27]
+// [0,2,7,10,19,24]
+// suffix = [25,20,17,8,3,0]

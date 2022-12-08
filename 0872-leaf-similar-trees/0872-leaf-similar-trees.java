47f2309cb7451467a89ffd3340b09454367ddfeb
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2022-12-08 10:45:17 +0530

    Time: 1 ms (77.58%), Space: 42.5 MB (9.55%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.java
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -35,3 +35,27 @@ class Solution {
         }
     }
 }
+
+
+// class Solution {
+    
+//     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+//         if(root1 == null || root2 == null) {
+//             return false;
+//         }
+//         StringBuilder sb1 = new StringBuilder();
+//         StringBuilder sb2 = new StringBuilder();
+//         helper(root1, sb1);
+//         helper(root2, sb2);
+//         return sb1.equals(sb2);
+//     }
+//     public void helper(TreeNode node, StringBuilder sb) {
+//         if(node != null) {
+//             if(node.left == null && node.right == null) {
+//                 sb.append(Integer.toString(node.val));
+//             }
+//             helper(node.left, sb);
+//             helper(node.right, sb);
+//         }
+//     }
+// }

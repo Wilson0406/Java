e2e13aaf7e54ba5afb89feb798f37a9ed1816acc
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2022-12-19 21:03:00 +0530

    Time: 3 ms (86.06%), Space: 43.4 MB (46.98%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1, j = num2.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while(i >= 0 || j >= 0 || carry != 0) {
+            if(i >= 0) carry += num1.charAt(i--) - '0';
+            if(j >= 0) carry += num2.charAt(j--) - '0';
+            sb.append(carry%10);
+            carry = carry/10;
+        }
+        return sb.reverse().toString();
+    }
+}

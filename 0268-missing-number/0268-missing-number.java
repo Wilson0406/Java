999ef6b0d84e5232d0685155af6b7ad3d33570c4
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2022-12-25 23:46:38 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (90.02%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        return n*(n+1)/2 - sum;
+    }
+}

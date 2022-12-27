d2531836d5b0eb8f02afe0556b2f4e906d2ef8a9
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2022-12-27 14:06:16 +0530

    Time: 15 ms (94.69%), Space: 54 MB (71.39%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
@@ -1,16 +1,11 @@
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
-        int sum = 0;
         int[] arr = new int[rocks.length];
 
         for(int i = 0; i < capacity.length; i++) {
-            // sum += capacity[i] - rocks[i];
             arr[i] = capacity[i] - rocks[i];
         }
         
-        // if(sum < additionalRocks) {
-        //     return rocks.length;
-        // }
         int ans = 0;
         Arrays.sort(arr);
         int count = 0;

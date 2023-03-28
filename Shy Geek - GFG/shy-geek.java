54bfbf03a0e57f327ac88b36c3bdfbf027405a04
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2023-03-28 14:54:01 +0530

    Added solution - LeetHub

---
+++ Shy Geek - GFG/shy-geek.java
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+        
+
+        while(t-->0){
+            Shop shop = new Shop();
+
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            long k = Long.parseLong(s[1]);
+
+            s = br.readLine().trim().split(" ");
+
+            for(String x: s){
+                shop.addChocolate(Integer.parseInt(x));
+            }
+            
+            ot.println(new Solution(shop).find(n, k));
+        }
+        ot.close();
+    }
+    
+    
+}
+class Shop{
+    private static List<Integer> chocoloates;
+    private static int countOfCalls;
+    Shop(){
+        chocoloates = new ArrayList<>();
+        countOfCalls = 0;
+    }
+    public static void addChocolate(int price){
+        chocoloates.add(price);
+    }
+
+    public static int get(int i){
+        countOfCalls++;
+        if(countOfCalls > 50 || i >= chocoloates.size() || i < 0)
+            return -1;
+        return chocoloates.get(i);
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+Instructions - 
+
+    1. 'shop' is object of class Shop.
+    2. User 'shop.get(int i)' to enquire about the price
+            of the i^th chocolate.
+    3. Every chocolate in shop is arranged in increasing order
+            i.e. shop.get(i) <= shop.get(i + 1) for all 0 <= i < n - 1
+*/
+class Solution{
+    
+    static Shop shop;
+    Solution(Shop shop){
+        this.shop = shop;
+    }
+    static long find(int n, long k){
+        // Return the number of chocolates Geek had
+        // enjoyed out of 'n' chocolates availabe in the
+        // 'shop'.
+        long result=0;
+        int lastIndex=n;
+        while(k>0){
+            int[] temp=getHighestChoclate(0,lastIndex-1,k);
+            if(temp[0]<0) break;
+            else{
+                result+=(k/(long)temp[1]);
+                k%=temp[1];
+                lastIndex=temp[0];
+            }
+        }
+        return result;
+    }
+    
+    public static int[] getHighestChoclate(int start,int end,long coins){
+        int result[]=new int[2];
+        Arrays.fill(result,-1);
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            int price=shop.get(mid);
+            if(price>coins) end=mid-1;
+            else{
+                result[0]=mid;
+                result[1]=price;
+                start=mid+1;
+            }
+        }
+        return result;
+    }
+
+}

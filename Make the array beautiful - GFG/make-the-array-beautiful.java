97b56c57804137bfffb5dfda7159ada4e856c5b1
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2023-04-08 20:28:29 +0530

    Added solution - LeetHub

---
+++ Make the array beautiful - GFG/make-the-array-beautiful.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            int[] arr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.makeBeautiful(arr);
+            for (int e : res) out.print(e + " ");
+            out.println();
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static ArrayList<Integer> makeBeautiful(int[] arr) {
+        Stack<Integer>st = new Stack<>();
+        ArrayList<Integer>lst = new ArrayList<>();
+        for(int i:arr)
+        {
+            if(st.empty())
+                st.push(i);
+            else
+            {
+                if((st.peek()>=0 && i<0) || st.peek()<0 && i>=0)
+                    st.pop();
+                else
+                    st.push(i);
+            }
+        }
+        while(!st.empty())
+        {
+            lst.add(st.peek());
+            st.pop();
+        }
+        Collections.reverse(lst);
+        return lst;
+    }
+}

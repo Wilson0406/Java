279cb0c0aae89228572875d9d6615e2295d09873
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-04-09 13:02:30 +0530

    Time: 1 ms, Memory: 46 MB - LeetHub

---
+++ move-zeroes/move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        if(nums == null || nums.length == 0) return;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        for(int i = j; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}

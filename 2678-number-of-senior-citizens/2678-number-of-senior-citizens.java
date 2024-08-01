6dcb19f569591076b903b5d097b3e681ccaa61a0
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-08-01 22:00:25 +0530

    Time: 1 ms (82.64%), Space: 42.6 MB (45.62%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -3,7 +3,6 @@ class Solution {
         int count = 0;
         for(String s : details) {
             char []ss = s.toCharArray();
-            // if(Integer.parseInt(ss[11])*10 + Integer.parseInt(ss[12]) > 60) {
             if((ss[11] - '0')*10 + (ss[12] - '0') > 60) {
                 count++;
             }

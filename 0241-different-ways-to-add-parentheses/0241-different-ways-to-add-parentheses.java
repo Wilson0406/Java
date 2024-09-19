435a81e9fe1fc8cf8753bc7b4fc25045a510f321
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-09-19 22:39:56 +0530

    Time: 2 ms (68.85%), Space: 42.8 MB (37.39%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -0,0 +1,36 @@
+class Solution {
+    Map<String, List<Integer>> map = new HashMap<>();
+    public List<Integer> diffWaysToCompute(String expression) {
+        List<Integer> res = new ArrayList<>();
+        
+        for(int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            
+            if(c == '+' || c == '-' || c == '*') {
+                String left = expression.substring(0, i);
+                String right = expression.substring(i + 1);
+                
+                List<Integer> l = diffWaysToCompute(left);
+                List<Integer> r = diffWaysToCompute(right);
+                
+                for(int n1 : l) {
+                    for(int n2 : r) {
+                        if(c == '+') {
+                            res.add(n1 + n2);
+                        } else if(c == '-') {
+                            res.add(n1 - n2);
+                        } else if(c == '*') {
+                            res.add(n1 * n2);
+                        }
+                    }
+                }
+            }
+        }
+        if(res.isEmpty()) {
+            res.add(Integer.parseInt(expression));
+        }
+        // map.put(expression, res);
+        
+        return res;
+    }
+}

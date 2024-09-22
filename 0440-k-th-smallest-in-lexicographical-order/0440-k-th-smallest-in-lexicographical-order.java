e188928c8a0ec2cb1298b2324a59f223eef97adc
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-09-23 00:03:37 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (46.51%) - LeetHub

---
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int calc(int n, long n1, long n2) {
+        int steps = 0;
+        while(n1 <= n) {
+            steps += Math.min(n + 1, n2) - n1;
+            n1 *= 10;
+            n2 *= 10;
+        }
+        return steps;
+    }
+    public int findKthNumber(int n, int k) {
+        int curr = 1;
+        k = k - 1;
+        while(k > 0) {
+            int steps = calc(n, curr, curr + 1);
+            if(steps <= k) {
+                curr += 1;
+                k -= steps;
+            } else {
+                curr *= 10;
+                k-= 1;
+            }
+        }
+        return curr;
+    }
+}

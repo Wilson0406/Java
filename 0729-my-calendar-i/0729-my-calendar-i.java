229e10e1f7797e050b325efafdd27080cb95568b
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-09-27 23:19:03 +0530

    Time: 1957 ms (5.01%), Space: 55.1 MB (6.50%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -1,14 +1,37 @@
+// class MyCalendar {
+//     List<int[]> calendar;
+//     public MyCalendar() {
+//         calendar = new ArrayList<>();
+//     }
+    
+//     public boolean book(int start, int end) {
+//         for(int[] iv : calendar) {
+//             if(iv[0] < end && start < iv[1]) return false;
+//         }
+//         calendar.add(new int[]{start, end});
+//         return true;
+//     }
+// }
 class MyCalendar {
-    List<int[]> calendar;
+    TreeMap<Integer, Integer> calendar;
     public MyCalendar() {
-        calendar = new ArrayList<>();
+        calendar = new TreeMap<>();
     }
     
     public boolean book(int start, int end) {
-        for(int[] iv : calendar) {
-            if(iv[0] < end && start < iv[1]) return false;
+        calendar.put(start, calendar.getOrDefault(start, 0) + 1);
+        calendar.put(end, calendar.getOrDefault(end, 0) - 1);
+        int count = 0;
+        for(int k : calendar.keySet()) {
+            count += calendar.get(k);
+            
+            if(count > 1) {
+                calendar.put(start, calendar.get(start) - 1);
+                calendar.put(end, calendar.get(end) + 1);
+                return false;
+                
+            }
         }
-        calendar.add(new int[]{start, end});
         return true;
     }
 }

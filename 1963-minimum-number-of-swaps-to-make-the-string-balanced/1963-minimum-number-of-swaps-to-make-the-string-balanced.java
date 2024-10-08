f472c3c78294fa0c8359b00b3c0204eb442630ec
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-10-08 22:48:10 +0530

    Time: 15 ms (65.33%), Space: 54.1 MB (76.92%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,16 +1,19 @@
 class Solution {
     public int minSwaps(String s) {
-        Stack<Character> st = new Stack<>();
+        // Stack<Character> st = new Stack<>();
         int swaps = 0;
         
         for(int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if(ch == '[') {
+                // st.push(ch);
                 swaps++;
             } else {
                 if(swaps > 0) swaps--;
+                // if(!st.isEmpty()) st.pop();
             }
         }
         return (swaps + 1) / 2;
+        // return (st.size() + 1) / 2;
     }
 }

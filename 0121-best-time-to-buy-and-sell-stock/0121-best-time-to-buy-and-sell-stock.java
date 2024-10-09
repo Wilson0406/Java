8ab31b0c109f48a389ce41b3a573e043f220d601
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-10-09 23:34:55 +0530

    Time: 2 ms (75.37%), Space: 61.5 MB (72.55%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int least = Integer.MAX_VALUE;
         int profit = 0;
-        int overall = 0;
-        for(int i=0;i<prices.length;i++){
-            if(prices[i]<least){
-                least = prices[i];
-            }
-            profit = prices[i] - least;
-            if(profit>overall){
-                overall = profit;
-            }
+        int buy = Integer.MAX_VALUE;
+        for(int i = 0; i < prices.length; i++) {
+            buy = Math.min(buy, prices[i]);
+            profit = Math.max(profit, prices[i] - buy);
         }
-        return overall;
+        return profit;
     }
 }

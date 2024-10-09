2007342c8ee8d7cf9e82130f41bbcf41db4f0177
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-10-10 01:21:58 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (77.49%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -11,16 +11,24 @@ class Solution {
         if(n < 0) return 0;
         if(n == 2) return Math.min(cost[0], cost[1]);
         
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        dp[0] = Math.min(cost[0], cost[1]);
+        // int[] dp = new int[n];
+        // Arrays.fill(dp, -1);
+        // dp[0] = Math.min(cost[0], cost[1]);
+        int prev1 = Math.min(cost[0], cost[1]);
+        int prev2 = 0;
+        int curr = 0;
         for(int i = 1; i < n; i++) {
-            if(i < 2) dp[i] = cost[i];
+            // if(i < 2) dp[i] = cost[i];
+            if(i < 2) curr = cost[i];
             else {
-                dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+                // dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+                curr = cost[i] + Math.min(prev1, prev2);
             }
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return Math.min(dp[n - 1], dp[n - 2]);
+        // return Math.min(dp[n - 1], dp[n - 2]);
+        return Math.min(prev1, prev2);
         // return Math.min(minCost(n - 1, cost, dp), minCost(n - 2, cost, dp));
     }
 }

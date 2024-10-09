d79ab5e6dfdd46e162bc0f11f8cd1a7f8d26792f
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-10-10 01:16:34 +0530

    Time: 1 ms (38.85%), Space: 43.1 MB (37.90%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -7,8 +7,20 @@ class Solution {
         return dp[i] = pay;
     }
     public int minCostClimbingStairs(int[] cost) {
-        int[] dp = new int[cost.length];
+        int n = cost.length;
+        if(n < 0) return 0;
+        if(n == 2) return Math.min(cost[0], cost[1]);
+        
+        int[] dp = new int[n];
         Arrays.fill(dp, -1);
-        return Math.min(minCost(cost.length - 1, cost, dp), minCost(cost.length - 2, cost, dp));
+        dp[0] = Math.min(cost[0], cost[1]);
+        for(int i = 1; i < n; i++) {
+            if(i < 2) dp[i] = cost[i];
+            else {
+                dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+            }
+        }
+        return Math.min(dp[n - 1], dp[n - 2]);
+        // return Math.min(minCost(n - 1, cost, dp), minCost(n - 2, cost, dp));
     }
 }

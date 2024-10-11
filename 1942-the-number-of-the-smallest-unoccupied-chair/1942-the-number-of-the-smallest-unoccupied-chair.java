91bf329ed32b00d84ac6ad798c96a28998a1d07d
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-10-12 01:17:56 +0530

    Time: 33 ms (97.74%), Space: 50.1 MB (46.89%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
@@ -1,25 +1,58 @@
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
         // Brute force
-        int chair[] = new int[times.length];
-        Arrays.fill(chair, -1);
+//         int chair[] = new int[times.length];
+//         Arrays.fill(chair, -1);
+//         int targetArrivalTime = times[targetFriend][0];
+//         Arrays.sort(times, (a, b) -> Integer.compare(a[0], b[0]));
+        
+//         for(int time[]: times) {
+//             int arrival = time[0];
+//             int depart = time[1];
+            
+//             for(int i = 0; i < times.length; i++) {
+//                 if(chair[i] <= arrival) {
+//                     chair[i] = depart;
+                    
+//                     if(arrival == targetArrivalTime) {
+//                         return i;
+//                     }
+//                 break;
+//                 }
+//             }
+//         }
+//         return -1;
+        
+        // Using Heaps
+        int n = times.length;
         int targetArrivalTime = times[targetFriend][0];
         Arrays.sort(times, (a, b) -> Integer.compare(a[0], b[0]));
         
-        for(int time[]: times) {
-            int arrival = time[0];
-            int depart = time[1];
+        PriorityQueue<int[]> occupied = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        PriorityQueue<Integer> free = new PriorityQueue<>();
+        
+        int chairNo = 0;
+        for(int i = 0; i < n; i++) {
+            int arrival = times[i][0];
+            int depart = times[i][1];
             
-            for(int i = 0; i < times.length; i++) {
-                if(chair[i] <= arrival) {
-                    chair[i] = depart;
-                    
-                    if(arrival == targetArrivalTime) {
-                        return i;
-                    }
-                break;
-                }
+            while(!occupied.isEmpty() && occupied.peek()[0] <= arrival) {
+                free.offer(occupied.poll()[1]);
             }
+            if(free.isEmpty()) {
+                occupied.offer(new int[]{depart, chairNo});
+                if(arrival == targetArrivalTime) {
+                    return chairNo;
+                }
+                chairNo++;
+            } else {
+                int leastChairNo = free.poll();
+                if(arrival == targetArrivalTime) {
+                    return leastChairNo;
+                }
+                occupied.offer(new int[]{depart, leastChairNo});
+            }
+            
         }
         return -1;
     }

f20da4ff74905dd62e7000f620cd57dbacf2ccba
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-10-12 12:16:43 +0530

    Time: 50 ms (70.92%), Space: 95.7 MB (12.77%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
@@ -1,17 +1,32 @@
 class Solution {
     public int minGroups(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
-        for(int interval[] : intervals) {
-            int arrival = interval[0];
-            int departure = interval[1];
+            // Using heaps
+//         Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+//         PriorityQueue<Integer> heap = new PriorityQueue<>();
+//         for(int interval[] : intervals) {
+//             int arrival = interval[0];
+//             int departure = interval[1];
             
-            if(!heap.isEmpty() && heap.peek() < arrival) {
-                heap.poll();
-            }
-            heap.add(departure);
+//             if(!heap.isEmpty() && heap.peek() < arrival) {
+//                 heap.poll();
+//             }
+//             heap.add(departure);
+//         }
+        
+//         return heap.size();
+        
+        // Line sweep
+        int line[] = new int[1000005];
+        int max = -1;
+        for(int interval[]: intervals) {
+            line[interval[0]]++;
+            line[interval[1] + 1]--;
         }
         
-        return heap.size();
+        for(int i = 1; i < 1000001; i++) {
+            line[i] += line[i - 1];
+            max = Math.max(max, line[i]);
+        }
+        return max;
     }
 }

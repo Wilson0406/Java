dfdd19011578ae43ff1235fd97b45b131e5076a2
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-10-14 23:38:25 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (5.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -5,17 +5,24 @@ class Solution {
         return dp[n] = Math.max(rob(nums, n - 1, dp), rob(nums, n - 2, dp) + nums[n]);
     }
     public int rob(int[] nums) {
-        if(nums.length < 0) return 0;
-        int[] dp = new int[nums.length + 1];
+        if(nums.length <= 0) return 0;
+        if(nums.length == 1) return nums[0];
+        if(nums.length == 2) return Math.max(nums[0], nums[1]);
+        // int[] dp = new int[nums.length + 1];
         // Arrays.fill(dp, -1);
         
-        dp[0] = 0;
-        dp[1] = nums[0];
-        for(int i = 1; i < nums.length; i++) {
-            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
+        // dp[0] = 0;
+        // dp[1] = nums[0];
+        int rob1 = 0;
+        int rob2 = 0;
+        for(int i = 0; i < nums.length; i++) {
+            int temp = Math.max(rob1 + nums[i], rob2);
+            rob1 = rob2;
+            rob2 = temp;
+            // dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
         }
-        return dp[nums.length];
-        
+        // return dp[nums.length];
+        return rob2;
         // return rob(nums, nums.length - 1, dp);
     }
 }

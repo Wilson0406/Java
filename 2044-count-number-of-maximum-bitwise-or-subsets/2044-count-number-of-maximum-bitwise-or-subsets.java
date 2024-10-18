c619cd0cdc89909e48bbe39276ceeb3ef67d062c
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-10-19 01:13:14 +0530

    Time: 9 ms (67.33%), Space: 40.8 MB (79.83%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
@@ -1,12 +1,15 @@
 class Solution {
     int res = 0;
     public int f(int idx, int or, int max, int nums[], int n) {
-        if(or == max) res++;
-        
-        for(int i = idx; i < n; i++) {
-            f(i + 1, or | nums[i], max, nums, n);
+        if(idx == n) {
+            if(or == max) return 1;
+            return 0;            
         }
-        return res;
+        
+        int take = f(idx + 1, or | nums[idx], max, nums, n);
+        int notTake = f(idx + 1, or, max, nums, n);
+        
+        return take + notTake;
     }
     public int countMaxOrSubsets(int[] nums) {
         int max = nums[0];

3ebd7ee5b3ca5ee6c9d79b906d95c8bfeae55da2
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-10-27 22:33:59 +0530

    Time: 10 ms (12.20%), Space: 55.2 MB (38.46%) - LeetHub

---
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -0,0 +1,31 @@
+class Solution {
+    int m, n;
+    public int solve(int i, int j, int[][] matrix, int[][] dp) {
+        if(i >= m || j >= n) return 0;
+        if(matrix[i][j] == 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int right = solve(i, j + 1, matrix, dp);
+        int diag = solve(i + 1, j + 1, matrix, dp);
+        int below = solve(i + 1, j, matrix, dp);
+        return dp[i][j] = 1 + Math.min(right, Math.min(diag, below));
+        
+    }
+    public int countSquares(int[][] matrix) {
+        m = matrix.length;
+        n = matrix[0].length;
+        int res = 0;
+        int dp[][] = new int[m + 1][n + 1];
+        for(int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == 1) {
+                    res += solve(i, j, matrix, dp);
+                }
+            }
+        }
+        return res;
+    }
+}

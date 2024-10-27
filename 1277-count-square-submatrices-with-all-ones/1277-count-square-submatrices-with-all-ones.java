feafb0299904e6553a22c1c86330a49bdeef8079
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-10-27 23:06:52 +0530

    Time: 7 ms (45.12%), Space: 54.6 MB (85.04%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,29 +1,49 @@
 class Solution {
-    int m, n;
-    public int solve(int i, int j, int[][] matrix, int[][] dp) {
-        if(i >= m || j >= n) return 0;
-        if(matrix[i][j] == 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
+    // Top Down
+//     int m, n;
+//     public int solve(int i, int j, int[][] matrix, int[][] dp) {
+//         if(i >= m || j >= n) return 0;
+//         if(matrix[i][j] == 0) return 0;
+//         if(dp[i][j] != -1) return dp[i][j];
         
-        int right = solve(i, j + 1, matrix, dp);
-        int diag = solve(i + 1, j + 1, matrix, dp);
-        int below = solve(i + 1, j, matrix, dp);
-        return dp[i][j] = 1 + Math.min(right, Math.min(diag, below));
+//         int right = solve(i, j + 1, matrix, dp);
+//         int diag = solve(i + 1, j + 1, matrix, dp);
+//         int below = solve(i + 1, j, matrix, dp);
+//         return dp[i][j] = 1 + Math.min(right, Math.min(diag, below));
         
-    }
+//     }
+//     public int countSquares(int[][] matrix) {
+//         m = matrix.length;
+//         n = matrix[0].length;
+//         int res = 0;
+//         int dp[][] = new int[m + 1][n + 1];
+//         for(int[] row : dp) {
+//             Arrays.fill(row, -1);
+//         }
+//         for(int i = 0; i < m; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 if(matrix[i][j] == 1) {
+//                     res += solve(i, j, matrix, dp);
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+    // Bottom up
     public int countSquares(int[][] matrix) {
-        m = matrix.length;
-        n = matrix[0].length;
         int res = 0;
-        int dp[][] = new int[m + 1][n + 1];
-        for(int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m + 1][n + 1];
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(matrix[i][j] == 1) {
-                    res += solve(i, j, matrix, dp);
+                if(i == 0 || j == 0) {
+                    dp[i][j] = matrix[i][j];
+                } else if(matrix[i][j] == 1) {
+                    dp[i][j] = 1 + Math.min(dp[i][j - 1], Math.min(dp[i - 1][j - 1], dp[i - 1][j]));
                 }
+                
+                res+= dp[i][j];
             }
         }
         return res;

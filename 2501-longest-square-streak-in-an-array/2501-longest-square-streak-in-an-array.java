7c78c2cf805150fb1fc792a3db6762c64fe247c1
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-10-28 22:43:02 +0530

    Time: 42 ms (78.13%), Space: 57.9 MB (83.33%) - LeetHub

--- 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.java
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.java
@@ -1,20 +1,41 @@
 class Solution {
+    // TC: O(nlogn)
+    // public int longestSquareStreak(int[] nums) {
+    //     Arrays.sort(nums);
+    //     int max = 0;
+    //     Map<Integer, Integer> map = new HashMap<>();
+    //     for(int num : nums) {
+    //         int root = (int)Math.sqrt(num);
+    //         if(root * root == num && map.containsKey(root)) {
+    //             map.put(num, map.get(root) + 1);
+    //         } else {
+    //             map.put(num, 1);
+    //         }
+    //         max = Math.max(max, map.get(num));
+    //     }
+    //     // System.out.println(max);
+    //     return max == 1 ? -1 : max;
+    // }
+    
+    // TC: O(N)
     public int longestSquareStreak(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        Arrays.sort(nums);
-        int count = -1;
-        for(int i = 0; i < n; i++) {
-            int ans = (int)Math.sqrt(nums[i]);
-            if(ans*ans == nums[i] && map.containsKey(ans)) {
-                map.put(nums[i], map.get(ans) + 1);
-                if(map.get(ans) + 1 > count) {
-                    count = map.get(ans) + 1;
-                }
-            } else {
-                map.put(nums[i],1);
-            }
+        Set<Integer> set = new HashSet<>();
+        int ans = 0;
+        for(int num : nums) {
+            set.add(num);
         }
-        return count;
+        for(int i = 0; i < nums.length; i++) {
+            long curr = nums[i];
+            int streak = 0;
+            while(set.contains((int)curr)) {
+                streak++;
+                if(curr * curr > 100000) {
+                    break;
+                }
+                curr *= curr;
+            }
+            ans = Math.max(ans, streak);
+        }
+        return ans < 2 ? -1 : ans;
     }
 }

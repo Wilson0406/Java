57cb3e8b6eb48719faba101723bf23d2cde186f2
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-11-06 00:54:07 +0530

    Time: 3 ms (70.13%), Space: 44 MB (70.83%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -15,6 +15,17 @@ class Solution {
         for(int row[] : dp) {
             Arrays.fill(row, -1);
         }
-        return solve(0, 0, triangle, dp);
+        
+        for(int j = 0; j < n; j++) {
+            dp[n - 1][j] = triangle.get(n - 1).get(j);
+        }
+        
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = i; j >= 0; j--) {
+                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i + 1][j], dp[i + 1][j + 1]);
+            }
+        }
+        return dp[0][0];
+        // return solve(0, 0, triangle, dp);
     }
 }

a7499460190e8e7267979879b223f5a50bd2738b
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-11-07 23:44:32 +0530

    Time: 31 ms (93.65%), Space: 57 MB (19.52%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.java
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.java
@@ -1,28 +1,40 @@
 class Solution {
-    public int largestCombination(int[] candidates) {
-        Map<Integer, String> map = new HashMap<>();
-        for(int i = 0; i < candidates.length; i++) {
-            StringBuilder sb = new StringBuilder();
-            if(Integer.toBinaryString(candidates[i]).length() < 24) {
-                int len = Integer.toBinaryString(candidates[i]).length();
-                int zero = 24 - len;
-                while(zero-- > 0) {
-                    sb.append('0');
-                }
+//     public int largestCombination(int[] candidates) {
+//         Map<Integer, String> map = new HashMap<>();
+//         for(int i = 0; i < candidates.length; i++) {
+//             StringBuilder sb = new StringBuilder();
+//             if(Integer.toBinaryString(candidates[i]).length() < 24) {
+//                 int len = Integer.toBinaryString(candidates[i]).length();
+//                 int zero = 24 - len;
+//                 while(zero-- > 0) {
+//                     sb.append('0');
+//                 }
                 
-            }
-            sb.append(Integer.toBinaryString(candidates[i]));
-            map.put(candidates[i], sb.toString());
-        }
+//             }
+//             sb.append(Integer.toBinaryString(candidates[i]));
+//             map.put(candidates[i], sb.toString());
+//         }
         
-        // int ans[] = new int[24];
-        
+//         int ans = 0;
+//         for(int j = 0; j < 24; j++) {
+//             int count = 0;
+//             for(int i = 0; i < candidates.length; i++) {
+//                 String binary = map.get(candidates[i]);
+//                 count += binary.charAt(j) - '0';
+//             }
+//             ans = Math.max(ans, count);
+//         }
+//         return ans;
+//     }
+    public int largestCombination(int[] candidates) {
         int ans = 0;
         for(int j = 0; j < 24; j++) {
             int count = 0;
             for(int i = 0; i < candidates.length; i++) {
-                String binary = map.get(candidates[i]);
-                count += binary.charAt(j) - '0';
+                if((candidates[i] & (1 << j)) != 0) {
+                    count++;
+                }
+                
             }
             ans = Math.max(ans, count);
         }

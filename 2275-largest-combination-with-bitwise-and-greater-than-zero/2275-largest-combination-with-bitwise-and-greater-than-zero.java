34d910bd1cfd9644edebe0c381ba3fd22130ae3e
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-11-07 23:32:16 +0530

    Time: 421 ms (9.52%), Space: 70.6 MB (5.18%) - LeetHub

---
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int largestCombination(int[] candidates) {
+        Map<Integer, String> map = new HashMap<>();
+        for(int i = 0; i < candidates.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            if(Integer.toBinaryString(candidates[i]).length() < 24) {
+                int len = Integer.toBinaryString(candidates[i]).length();
+                int zero = 24 - len;
+                while(zero-- > 0) {
+                    sb.append('0');
+                }
+                
+            }
+            sb.append(Integer.toBinaryString(candidates[i]));
+            map.put(candidates[i], sb.toString());
+        }
+        
+        int ans[] = new int[24];
+        int count = 0;
+        for(int j = 0; j < 24; j++) {
+            for(int i = 0; i < candidates.length; i++) {
+                String binary = map.get(candidates[i]);
+                ans[j] += binary.charAt(j) - '0';
+            }
+            count = Math.max(count, ans[j]);
+        }
+        // for(int i = 0; i < 24; i++) {
+        //     System.out.println()
+        // }
+        return count;
+    }
+}

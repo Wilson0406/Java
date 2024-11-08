11d7d7702c44ef0ea020d0158d970a69222752d5
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-11-08 23:17:37 +0530

    Time: 2 ms (100.00%), Space: 63.5 MB (12.56%) - LeetHub

---
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
@@ -0,0 +1,20 @@
+class Solution {
+    // Brute Force
+    public int[] getMaximumXor(int[] nums, int maximumBit) {
+        int maxK = (1 << maximumBit) - 1;
+        int maxXor = maxK;
+        // System.out.println(maxXor);
+        int n = nums.length;
+        int[] ans = new int[n];
+        int[] prefix = new int[n];
+        prefix[0] = nums[0];
+        for(int i = 1; i < n; i++) {
+            prefix[i] = nums[i] ^ prefix[i - 1];
+        }
+        int j = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            ans[j++] = (prefix[i] ^ maxK);
+        }
+        return ans;
+    }
+}

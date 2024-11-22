3e281b3ddc6d197f9de23073e44317ec4c34edf1
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-11-22 19:20:15 +0530

    Time: 54 ms (28.09%), Space: 61.3 MB (26.12%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.java
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.java
@@ -4,16 +4,16 @@ class Solution {
         int n = matrix[0].length;
         
         int maxRows = 0;
-        for(int[] currRow : matrix) {
+        for(int[] currRow : matrix) { // O(m * (n + m * n))
             int[] inverted = new int[n];
             
-            for(int col = 0; col < currRow.length; col++) {
+            for(int col = 0; col < currRow.length; col++) { // O(n)
                 inverted[col] = currRow[col] == 0 ? 1 : 0;
             }
             
             int count = 0;
-            for(int[] row : matrix) {
-                if(Arrays.equals(row, currRow) || Arrays.equals(row, inverted)) {
+            for(int[] row : matrix) { // O(m * n)
+                if(Arrays.equals(row, currRow) || Arrays.equals(row, inverted)) { // O(n)
                     count++;
                 }
             }

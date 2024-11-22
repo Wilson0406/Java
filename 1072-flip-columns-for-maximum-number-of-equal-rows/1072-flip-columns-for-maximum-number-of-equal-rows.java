7b6160ee04ba9108aead95ef933ee75f500d711a
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-11-22 19:25:36 +0530

    Time: 78 ms (6.74%), Space: 61.1 MB (30.60%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.java
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.java
@@ -1,24 +1,44 @@
 class Solution {
+//     public int maxEqualRowsAfterFlips(int[][] matrix) {
+//         int m = matrix.length;
+//         int n = matrix[0].length;
+        
+//         int maxRows = 0;
+//         for(int[] currRow : matrix) { // O(m * (n + m * n))
+//             int[] inverted = new int[n];
+            
+//             for(int col = 0; col < currRow.length; col++) { // O(n)
+//                 inverted[col] = currRow[col] == 0 ? 1 : 0;
+//             }
+            
+//             int count = 0;
+//             for(int[] row : matrix) { // O(m * n)
+//                 if(Arrays.equals(row, currRow) || Arrays.equals(row, inverted)) { // O(n)
+//                     count++;
+//                 }
+//             }
+            
+//             maxRows = Math.max(maxRows, count);
+//         }
+//         return maxRows;
+//     }
+    
     public int maxEqualRowsAfterFlips(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        
+        HashMap<String, Integer> map = new HashMap<>();
         int maxRows = 0;
-        for(int[] currRow : matrix) { // O(m * (n + m * n))
-            int[] inverted = new int[n];
-            
-            for(int col = 0; col < currRow.length; col++) { // O(n)
-                inverted[col] = currRow[col] == 0 ? 1 : 0;
+        for(int[] currRow : matrix) {
+            String rowNature = "";
+            int first = currRow[0];
+            for(int col = 0; col < currRow.length; col++) {
+                rowNature += first == currRow[col] ? "s" : "c"; // s -> same, c -> change
             }
-            
-            int count = 0;
-            for(int[] row : matrix) { // O(m * n)
-                if(Arrays.equals(row, currRow) || Arrays.equals(row, inverted)) { // O(n)
-                    count++;
-                }
-            }
-            
-            maxRows = Math.max(maxRows, count);
+            map.put(rowNature, map.getOrDefault(rowNature, 0) + 1);
+        }
+        
+        for(Map.Entry<String,Integer> entry : map.entrySet()) {
+            maxRows = Math.max(maxRows, entry.getValue());
         }
         return maxRows;
     }

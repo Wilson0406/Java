d71ef31325bc52d0b92ac30e7672d8a8a4cd2377
Wilson Vidyut Doloy <wilsonvidyut@gmail.com>
2024-11-23 17:24:47 +0530

    Time: 31 ms (10.16%), Space: 79.2 MB (80.22%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -0,0 +1,52 @@
+class Solution {
+    public char[] reverseRow(char[] row) {
+        int s = 0, e = row.length - 1;
+        while(s < e) {
+            char temp = row[s];
+            row[s] = row[e];
+            row[e] = temp;
+            s++;
+            e--;
+        }
+        return row;
+    }
+    public char[][] rotateTheBox(char[][] box) {
+        int m = box.length;
+        int n = box[0].length;
+        char[][] ans = new char[n][m];
+        
+        // transpose
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                ans[j][i] = box[i][j];
+            }
+        }
+        
+        // reverse row
+        for(int i = 0; i < n; i++) {
+            ans[i] = reverseRow(ans[i]);
+        }
+        
+        // apply gravity
+        for(int j = 0; j < m; j++) {
+            for(int i = n - 1; i >= 0; i--) {
+                if(ans[i][j] == '.') {
+                    int stoneRow = -1;
+                    for(int k = i - 1; k >= 0; k--) {
+                        if(ans[k][j] == '*') break;
+                        else if(ans[k][j] == '#') {
+                            stoneRow = k;
+                            break;
+                        }
+                    }
+                    if(stoneRow != -1) {
+                        ans[i][j] = '#';
+                        ans[stoneRow][j] = '.';
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
